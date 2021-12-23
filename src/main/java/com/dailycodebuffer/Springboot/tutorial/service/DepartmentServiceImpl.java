3ab6e98510af5f06251cafbd7dc0992fd77fbf2e
Suraj Tamang <surajmoktan40@gmail.com>
2021-12-23 14:02:20 -0500

    Initial commit

---
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -0,0 +1,18 @@
+package com.dailycodebuffer.Springboot.tutorial.service;
+
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+}
